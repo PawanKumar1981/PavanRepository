e44b0f60c1b2db215df5e6ac2f19ca5013f1bd4d
gangonepavan <pawanautomationtesting@gmail.com>
2018-10-21 08:54:30 +0530

    test

---
+++ CollectionExample/src/CollectionExm/StudentArrayList.java
@@ -0,0 +1,13 @@
+package CollectionExm;
+
+public class StudentArrayList {
+	int Rollno;
+	String name;
+	int age;
+	StudentArrayList(int Rollno,String name,int age){
+		this.Rollno=Rollno;
+		this.name=name;
+		this.age=age;
+	}
+
+}
